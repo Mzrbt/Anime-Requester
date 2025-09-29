bcf90dad31850cb66d173c27b83b3251295ec71c
jabet241 <marin.jabet@etu.unicaen.fr>
2025-09-29 11:30:30 +0200

    Requêtes API

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const url = 'https://anime-db.p.rapidapi.com/anime/by-id/1';
+let url = 'https://anime-db.p.rapidapi.com/anime/by-id/';
+
+var btnID = document.getElementById("button_ID");
+var btnNAME = document.getElementById("button_NAME");
+var btnRANK = document.getElementById("button_RANK")
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -17,11 +22,27 @@ async function fetchData() {
     }
 }
 
+function checkName() {
+    var NAME = document.getElementById("valeur_NAME").value;
+    url = 'https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=' + NAME + '&sortBy=ranking&sortOrder=asc';
+    return fetchData(url, options);
+}
+
+function checkID() {
+    var ID = document.getElementById("valeur_ID").value;
+    url = 'https://anime-db.p.rapidapi.com/anime/by-id/' + ID;
+    return fetchData(url, options);
+}
+
+function checkRank() {
+    var RANK = document.getElementById("valeur_RANK").value;
+    url = 'https://anime-db.p.rapidapi.com/anime/by-ranking/' + RANK;
+    return fetchData(url, options);
+}
+
+btnID.addEventListener("click", checkID);
+btnNAME.addEventListener("click", checkName);
+btnRANK.addEventListener("click", checkRank);
+
 fetchData();
 
-/*
-window.addEventListener("load", () =>{
-    function sendData() {
-
-    }
-})*/
