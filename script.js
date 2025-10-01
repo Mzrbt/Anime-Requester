21cb722f1ec6bf8b8bde52e82ceea9d1d75383cb
jabet241 <marin.jabet@etu.unicaen.fr>
2025-10-01 09:53:43 +0200

    Fonctionnement de l'affichage

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 let url = 'https://anime-db.p.rapidapi.com/anime/by-id/';
-let txt = document.getElementById("p");
 let btn = document.getElementById("button_choixParam");
 
 const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'dda0b0e7abmsh635c70595822706p1a5847jsnab7042edc621',
+		'x-rapidapi-key': '2893250d38msha2693fc658faba8p1d3d7ajsn469620810024',
 		'x-rapidapi-host': 'anime-db.p.rapidapi.com'
 	}
 };
+
+
+function createCard(result) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+        <h2>${result.title}</h2>    
+        <img src="${result.image}"></img>
+        <p><strong>Rating:</strong> ${result.rating}</p>
+        <p><strong>Synopsis:</strong> ${result.synopsis}</p>
+    `;
+    document.body.appendChild(card);
+}
+
 
 async function fetchData() {
     try {
         const response = await fetch(url, options);
         const result = await response.text();
-        console.log(result);
+        const data = JSON.parse(result);
+        console.log(data);
+        console.log(data.data.length);
+        
+        try{
+            for (let i = 0; i < data.data.length; i++){
+            console.log(data.data[i]);
+            createCard(data.data[i]);
+            }
+        } catch (error){
+            createCard(data);
+        }
+        
     } catch (error) {
         console.error(error);
     }
@@ -40,4 +65,3 @@ function checkButton() {
 btn.addEventListener("click", checkButton);
 
 fetchData();
-
